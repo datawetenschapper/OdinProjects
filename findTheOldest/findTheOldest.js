a014d8d0f87a2fac1d9ec440b991a15779800405
Data <datawetenschapper@gmail.com>
2021-04-19 15:34:45 +0200

    changed the oldest person again a bit

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,10 +1,14 @@
-let findTheOldest = function(oldest, youngest) {
-    const young = (youngest.yearOfDeath - youngest.yearOfBirth);
-    console.log(young);
-    const old = (oldest.yearOfDeath - oldest.yearOfBirth);
-    console.log(old)
-    return (old > young ? 1 : -1);
+let findTheOldest = function (array) {
+    return array.reduce((youngest, oldest) => {
+        if (!this.yearOfDeath){
+            this.yearOfDeath = new Date().getFullYear();
+            console.log(this + 'death edited')
+        }
+        const young = (youngest.yearOfDeath - youngest.yearOfBirth);
+        console.table(young + youngest.name);
+        const old = (oldest.yearOfDeath - oldest.yearOfBirth);
+        console.log(old + oldest.name)
+        return old > young ? oldest: youngest ;
+    })
 };
 module.exports = findTheOldest
-
-

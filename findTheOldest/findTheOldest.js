e71a76b1e66ff037eb3d2f449258d0a24602eed1
datawetenschapper <70944743+datawetenschapper@users.noreply.github.com>
2021-04-19 11:43:42 +0200

    editing findtheoldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-let findTheOldest = function(people) {
-    age = people.yearOfDeath - people.yearOfBirth; 
-}
-
+let findTheOldest = function(oldest, youngest) {
+    const young = (youngest.yearOfDeath - youngest.yearOfBirth);
+    console.log(young);
+    const old = (oldest.yearOfDeath - oldest.yearOfBirth);
+    console.log(old)
+    return (old > young ? 1 : -1);
+};
 module.exports = findTheOldest
+
+
